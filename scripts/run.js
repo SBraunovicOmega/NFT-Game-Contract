c455f032e7955cca280cfccb9e216d67b62d2411
Stefan <stefan.braunovic@vegait.rs>
2022-08-15 14:12:39 +0200

    Setup data for character NFTs

--- scripts/run.js
+++ scripts/run.js
@@ -1,13 +1,20 @@
 
 const main = async ()=>{
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Bečić", "Abazović", "Krivokapić"], //Names
+        ["https://i.imgur.com/Nzl5gRs.jpg",  //Images
+        "https://i.imgur.com/mCSyZIv.jpg",
+        "https://i.imgur.com/d3GW8Uh.jpg"],
+        [100,200,300], // HP values
+        [100,50,25] // Attack damage values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
 }
 
 
-const runMian = async ()=>{
+const runMain = async ()=>{
     try{
         await main();
         process.exit(0)
@@ -18,4 +25,4 @@ const runMian = async ()=>{
 }
 
 
-runMian()
+runMain()
